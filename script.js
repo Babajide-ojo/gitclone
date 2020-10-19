56e8849d5193ccf44bff2efd1583209172853fbe
Victoria <kelzvictoria@gmail.com>
2020-10-19 16:42:20 +0100

    successfully added

---
+++ script.js
@@ -0,0 +1,7 @@
+const React = require("react")
+
+const ReactDOM = require("react-dom")
+
+ReactDOM.render(<p>Hallo</p>, document.getElementById('root'));
+
+//Click the "index.htm" tab to see the container "sandy".
